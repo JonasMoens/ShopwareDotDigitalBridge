fe8383a2fa29cff2cd712cf11819112d9909da5b
JonasMoens <32325942+JonasMoens@users.noreply.github.com>
2019-05-22 11:54:15 +0200

    Add files via upload

---
+++ bridge2cart/config.php
@@ -0,0 +1,10 @@
+<?php
+/*                       ATTENTION!
++------------------------------------------------------------------------------+
+| By our Terms of Use you agreed not to change, modify, add, or remove portions|
+| of Bridge Script source code                                                 |
++-----------------------------------------------------------------------------*/
+
+
+define("M1_TOKEN", "d690e8b3f56cc86e4e124ac4ca2898a9");
+
